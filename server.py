3d79e9bfd1ffcf1ea3ba93ce684bb1cfe83a1f31
Alex Markin <alexndr86@gmail.com>
2019-02-07 20:42:45 +0300

    httpserver + httpworker

--- server.py
+++ server.py
@@ -3,81 +3,13 @@ import os
 import socket
 import typing
 
+from threading import Thread
+from queue import Queue, Empty
+from request import Request
+from response import Response
+
 
 SERVER_ROOT = os.path.abspath("www")
-
-HOST = "127.0.0.1"
-PORT = 9000
-RESPONSE = b"""\
-HTTP/1.1 200 OK
-Content-type: text/html
-Content-length: 15
-
-<h1>Hello!</h1>""".replace(
-    b"\n", b"\r\n"
-)
-
-FILE_RESPONSE_TEMPLATE = """\
-HTTP/1.1 200 OK
-Content-type: {content_type}
-Content-length: {content_length}
-
-""".replace(
-    "\n", "\r\n"
-)
-
-BAD_REQUEST_RESPONSE = b"""\
-HTTP/1.1 400 Bad Request
-Content-type: text/plain
-Content-length: 11
-
-Bad Request""".replace(
-    b"\n", b"\r\n"
-)
-
-NOT_FOUND_RESPONSE = b"""\
-HTTP/1.1 404 Not Found
-Content-type: text/plain
-Content-length: 9
-
-Not Found""".replace(
-    b"\n", b"\r\n"
-)
-
-METHOD_NOT_ALLOWED_RESPONSE = b"""\
-HTTP/1.1 405 Method Not Allowed
-Content-type: text/plain
-Content-length: 17
-
-Methon Not Allowed""".replace(
-    b"\n", b"\r\n"
-)
-
-
-def iter_lines(
-    sock: socket.socket, bufsize: int = 16384
-) -> typing.Generator[bytes, None, bytes]:
-    """Given a socket, read all the individual CRLF-separated lines
-    and yield each one until an empty one is found.  Returns the
-    remainder after the empty line.
-    """
-    buff = b""
-    while True:
-        data = sock.recv(bufsize)
-        if not data:
-            return b""
-
-        buff += data
-        while True:
-            try:
-                i = buff.index(b"\r\n")
-                line, buff = buff[:i], buff[i + 2 :]
-                if not line:
-                    return buff
-
-                yield line
-            except IndexError:
-                break
 
 
 def serve_file(sock: socket.socket, path: str) -> None:
@@ -90,12 +22,12 @@ def serve_file(sock: socket.socket, path: str) -> None:
 
     abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
     if not abspath.startswith(SERVER_ROOT):
-        sock.sendall(NOT_FOUND_RESPONSE)
+        response = Response(status="404 Not Found", content="Not Found")
+        response.send(sock)
         return
 
     try:
         with open(abspath, "rb") as f:
-            stat = os.fstat(f.fileno())
             content_type, encoding = mimetypes.guess_type(abspath)
             if content_type is None:
                 content_type = "application/octet-stream"
@@ -103,76 +35,116 @@ def serve_file(sock: socket.socket, path: str) -> None:
             if encoding is not None:
                 content_type += f"; charset={encoding}"
 
-            response_headers = FILE_RESPONSE_TEMPLATE.format(
-                content_type=content_type, content_length=stat.st_size
-            ).encode("ascii")
-
-            sock.sendall(response_headers)
-            sock.sendfile(f)
+            response = Response(status="200 OK", body=f)
+            response.headers.add("content-type", content_type)
+            response.send(sock)
+            return
     except FileNotFoundError:
-        sock.sendall(NOT_FOUND_RESPONSE)
+        response = Response(status="404 Not Found", content="Not Found")
+        response.send(sock)
         return
 
 
-class Request(typing.NamedTuple):
-    method: str
-    path: str
-    headers: typing.Mapping[str, str]
+class HTTPWorker(Thread):
+    def __init__(self, connection_queue: Queue) -> None:
+        super().__init__(daemon=True)
 
-    @classmethod
-    def from_socket(cls, sock: socket.socket) -> "Request":
-        """Read and parse the request from a socket object.
+        self.connection_queue = connection_queue
+        self.running = False
 
-        Raises:
-          ValueError: When the request cannot be parsed.
-        """
-        lines = iter_lines(sock)
+    def stop(self) -> None:
+        self.running = False
 
-        try:
-            request_line = next(lines).decode("ascii")
-        except StopIteration:
-            raise ValueError("Request line missing.")
-
-        try:
-            method, path, _ = request_line.split(" ")
-        except ValueError:
-            raise ValueError(f"Malformed request line {request_line!r}.")
-
-        headers = {}
-        for line in lines:
+    def run(self) -> None:
+        self.running = True
+        while self.running:
             try:
-                name, _, value = line.decode("ascii").partition(":")
-                headers[name.lower()] = value.lstrip()
-            except ValueError:
-                raise ValueError(f"Malformed header line {line!r}.")
-        return cls(method=method.upper(), path=path, headers=headers)
+                client_sock, client_addr = self.connection_queue.get(timeout=1)
+            except Empty:
+                continue
 
+            try:
+                self.handle_client(client_sock, client_addr)
+            except Exception as e:
+                print(f"Unhandled error: {e}")
+                continue
+            finally:
+                self.connection_queue.task_done()
 
-# By default, socket.socket creates TCP sockets.
-with socket.socket() as server_sock:
-    # This tells the kernel to reuse sockets that are in `TIME_WAIT` state.
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # This tells the socket what address to bind to.
-    server_sock.bind((HOST, PORT))
-
-    # 0 is the number of pending connections the socket may have before
-    # new connections are refused.  Since this server is going to process
-    # one connection at a time, we want to refuse any additional connections.
-    server_sock.listen(0)
-    print(f"Listening on {HOST}:{PORT}...")
-
-    while True:
-        client_sock, client_addr = server_sock.accept()
-        print(f"New connection from {client_addr}.")
+    def handle_client(
+        self, client_sock: socket.socket, client_addr: typing.Tuple[str, int]
+    ) -> None:
         with client_sock:
             try:
                 request = Request.from_socket(client_sock)
+                if "100-continue" in request.headers.get("expect", ""):
+                    response = Response(status="100 Continue")
+                    response.send(client_sock)
+
+                try:
+                    content_length = int(request.headers.get("content-length", 0))
+                except ValueError:
+                    content_length = 0
+
+                if content_length:
+                    body = request.body.read(content_length)
+                    print("Request body", body)
+
                 if request.method != "GET":
-                    client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
-                    continue
+                    response = Response(
+                        status="405 Method Not Allowed", content="Method Not Allowed"
+                    )
+                    response.send(client_sock)
+                    return
 
                 serve_file(client_sock, request.path)
             except Exception as e:
                 print(f"Failed to parse request: {e}")
-                client_sock.sendall(BAD_REQUEST_RESPONSE)
+                response = Response(status="400 Bad request", content="Bad request")
+                response.send(client_sock)
+
+
+class HTTPServer:
+    def __init__(self, host="127.0.0.1", port=9000, worker_count=16) -> None:
+        self.host = host
+        self.port = port
+        self.worker_count = worker_count
+        self.worker_backlog = worker_count * 8
+        self.connection_queue = Queue(self.worker_backlog)
+
+    def serve_forever(self) -> None:
+        workers = []
+        for _ in range(self.worker_count):
+            worker = HTTPWorker(self.connection_queue)
+            worker.start()
+            workers.append(worker)
+
+        # By default, socket.socket creates TCP sockets.
+        with socket.socket() as server_sock:
+            # This tells the kernel to reuse sockets that are in `TIME_WAIT` state.
+            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # This tells the socket what address to bind to.
+            server_sock.bind((self.host, self.port))
+
+            # 0 is the number of pending connections the socket may have before
+            # new connections are refused.  Since this server is going to process
+            # one connection at a time, we want to refuse any additional connections.
+            server_sock.listen(self.worker_backlog)
+            print(f"Listening on {self.host}:{self.port}...")
+
+            while True:
+                try:
+                    self.connection_queue.put(server_sock.accept())
+                except KeyboardInterrupt:
+                    break
+
+        for worker in workers:
+            worker.stop()
+
+        for worker in workers:
+            worker.join(timeout=30)
+
+
+server = HTTPServer()
+server.serve_forever()
